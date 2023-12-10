b443735b855a5122cef96708b83112878a02598c
GitHub Action <action@github.com>
2023-12-10 07:12:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/trgpnt/javaclassdecorator/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/trgpnt/javaclassdecorator/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.trgpnt.javaclassdecorator.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
